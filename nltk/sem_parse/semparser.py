369e7cf3cff76d138bb79815eac79cbbe80d6695
Jake Vasilakes <jvasilakes@gmail.com>
2015-04-21 20:48:18 +0100

    Make accord with PEP8

--- nltk/sem_parse/semparser.py
+++ nltk/sem_parse/semparser.py
@@ -5,25 +5,22 @@
 # URL: <http://nltk.org/>
 # For license information, see LICENSE.TXT
 
-'''
+"""
 Semantic parser which builds MRs using nltk.ccg
 
 Data:
 Relies on nltk:grammars/ccg_grammars/lex.ccg
 Get it from <homepages.inf.ed.ac.uk/s1420849/lex.ccg>
 and put it in, e.g. $HOME/nltk_data/grammars/ccg_grammars/lex.ccg
-'''
+"""
 from __future__ import print_function, division, unicode_literals
 
 import sys
-import random
 
 from nltk.data import load
 from nltk.ccg import lexicon, chart
-
-from nltk.corpus import dependency_treebank
-from nltk.parse import dependencygraph as dg
-from nltk.parse import projectivedependencyparser as dp
+from nltk.parse import (dependencygraph as dg,
+                        projectivedependencyparser as dp)
 
 
 class CCGSemParser(object):
@@ -37,10 +34,10 @@ class CCGSemParser(object):
         module and a custom lexicon, lex.ccg.
 
     Usage example:
-        from nltk.sem_parse import semparser
+        from nltk.sem_parse.semparser import CCGSemParser
         from nltk.ccg import chart
-        SP = semparser.CCGSemParser("grammars/ccg_grammars/lex.ccg")
-        parses = SP.parse("I went home")
+        SemParser = CCGSemParser("grammars/ccg_grammars/lex.ccg")
+        parses = SemParser.parse("I went home")
         chart.printCCGDerivation(next(parses))
     """
 
@@ -87,31 +84,33 @@ class DependencySemParser(object):
         function. Output parses are NLTK Trees.
 
         from nltk.sem_parse.semparser import DependencySemParser
-        SemParser = DependencySemParser(train='path/to/training_data', test='path/to/test_data')
+        SemParser = DependencySemParser(train='path/to/training_data',
+                                        test='path/to/test_data')
         SemParser.train()
         parses = SemParser.parse(['I', 'went', 'home', '.'])
         for parse in parses:
             print parse
     """
 
-    def __init__(self, train='data/en-ud-train.conllu', test='data/en-ud-dev.conllu'):
+    def __init__(self, train=None, test=None):
         """
         param str train: path to training data file.
         param str test: path to test data file.
         """
-        self._training_data = self._get_train_data(train)
-        self._testing_data = self._get_test_data(test)
         self._parser = dp.ProbabilisticProjectiveDependencyParser()
+        if train:
+            self._training_data = self._get_train_data(train)
+        if test:
+            self._testing_data = self._get_test_data(test)
 
     def _get_train_data(self, data_file):
         """
-        Read in the training data and create DependencyGraphs from 
+        Read in the training data and create DependencyGraphs from
         each sentence. These are used by the self._parser.train() method.
 
         param str data_file: path to training data file.
         """
-        #data = dependency_treebank.raw().split('\n\n')  #data1
-        data = open(data_file).read().split('\n\n')  #data2
+        data = open(data_file).read().split('\n\n')
         data = [dg.DependencyGraph(entry) for entry in data]
         return data
 
@@ -122,7 +121,6 @@ class DependencySemParser(object):
         param str data_file: path to test data file.
         """
         data = open(data_file).read().split('\n\n')
-        #data = open('data/en-ud-train.conllu').read().split('\n\n')  #data2
         data = [entry for entry in data if entry]
         gold_pairs = []
         for entry in data:
@@ -138,7 +136,8 @@ class DependencySemParser(object):
         :param str entry: a sentence and it's associated annotations.
         """
         sentence = entry.split('\n')
-        if len(sentence) <= 1: return []
+        if len(sentence) <= 1:
+            return []
         sentence = [field.split('\t') for field in sentence]
         # 0 index is the word for data1, 1 index is the word for data2.
         sentence = [field[1] for field in sentence]
@@ -159,14 +158,14 @@ class DependencySemParser(object):
         data and train the parser.
         """
         self._parser.train(self._training_data)
-        
+
     def test(self):
         """
         Using the trained projective dependency parser,
         parse the test data and compute accuracy.
         """
         correct = 0
-        for (i,(sent,gold_parse)) in enumerate(self._testing_data):
+        for (i, (sent, gold_parse)) in enumerate(self._testing_data):
             sys.stderr.write("{0}/{1}\r" .format(i, len(self._testing_data)))
 
             try:
@@ -175,26 +174,25 @@ class DependencySemParser(object):
                 print("ZeroDivisionError raised parsing: {0}" .format(sent))
             except:
                 print("\n## Results ##")
-                print("{0}/{1} sentences parsed correctly" 
-                       .format(correct, len(self._testing_data)))
-                print("Accuracy: {0}" 
-                       .format((float(correct)/len(self._testing_data))))
+                print("{0}/{1} sentences parsed correctly"
+                      .format(correct, len(self._testing_data)))
+                print("Accuracy: {0}"
+                      .format((float(correct)/len(self._testing_data))))
                 return
 
             if hyp:
                 print("GOLD\n{0}" .format(gold_parse))
                 for parse in hyp:
                     print("HYP\n{0}\n" .format(parse))
-                raw_input()
 
             if gold_parse in hyp:
                 correct += 1
 
         print("\n## Results ##")
-        print("{0}/{1} sentences parsed correctly" 
-               .format(correct, len(self._testing_data)))
-        print("Accuracy: {0}" 
-               .format((float(correct)/len(self._testing_data))))
+        print("{0}/{1} sentences parsed correctly"
+              .format(correct, len(self._testing_data)))
+        print("Accuracy: {0}"
+              .format((float(correct)/len(self._testing_data))))
 
 
 def CCGDemo():
@@ -208,7 +206,8 @@ def CCGDemo():
 
 def DepDemo():
     print("Building parser...")
-    semparser = DependencySemParser()
+    semparser = DependencySemParser(train='data/en-ud-train.conllu',
+                                    test='data/en-ud-train.conllu')
     print("Training...")
     semparser.train()
     print("Testing...")
