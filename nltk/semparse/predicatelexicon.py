8201437ed3ed83c1db4ce72aaed8e777f6b07ef3
Jake Vasilakes <jvasilakes@gmail.com>
2015-07-13 23:18:01 -0500

    Key predlex by word and category.

--- nltk/semparse/predicatelexicon.py
+++ nltk/semparse/predicatelexicon.py
@@ -5,10 +5,16 @@ from nltk.sem.logic import Expression
 
 class PredicateLexicon(defaultdict):
     """
-    Lexicon that maps CCG categories and words to logical forms.
+    Lexicon that maps words and CCG categories to logical forms.
     If the logical form of a word is known, it is returned.
     Otherwise, user can specify a word and a category to get
     a list of possible logical forms for that word with that category.
+
+    Predicate lexicon file format:
+    #WORDS
+    word :: category :: predicate 
+    #CATEGORIES
+    category :: predicate
     """
     
     _lexpr = Expression.fromstring
@@ -30,8 +36,8 @@ class PredicateLexicon(defaultdict):
                 key = line[1:].strip()
                 continue
             if key == 'WORDS':
-                (word, pred) = line.strip().split(' :: ')
-                predLex[word].append(cls._lexpr(pred))
+                (word, cat, pred) = line.strip().split(' :: ')
+                predLex[(word, cat)].append(cls._lexpr(pred))
             elif key == 'CATEGORIES':
                 (cat, pred) = line.strip().split(' :: ')
                 predLex.categories[cat].append(cls._lexpr(pred))
@@ -44,13 +50,19 @@ class PredicateLexicon(defaultdict):
         self.categories = defaultdict(list)
 
     def get(self, word=None, category=None):
-        if not word:
-            return self.categories[category]
+        if not word and not category:
+            return set()
+        elif not word and category:
+            return set(self.categories[category])
+        elif word and not category:
+            preds = [self[(w,c)] for (w,c) in self.keys() if word == w]
+            preds = set(reduce(list.__add__, preds, []))
+            return preds
+        elif (word, category) in self.keys():
+            return set(self[(word, category)])
         else:
-            if word in self.keys():
-                return self[word]
-            else:
-                if len(word) == 1:
-                    word = "_{0}".format(word)
-                return [self._lexpr(expr.__str__().format(word))
-                        for expr in self.categories[category]]
+            if len(word) == 1:
+                word = "_{0}".format(word)
+            preds = [self._lexpr(expr.__str__().format(word))
+                     for expr in self.categories.get(category, [])]
+            return set(preds)
